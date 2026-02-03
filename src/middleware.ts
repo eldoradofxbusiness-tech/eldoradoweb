2ce7eb7d91e1f3a003048f1753250572769d5151
dlrodev92 <dlrdev92@gmail.com>
2026-02-03 11:09:54 +0000

    middleware routes updated performance

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,34 +2,38 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
-  const hostname = context.url.hostname; 
+  const hostname = context.url.hostname;
+  const pathname = url.pathname;
 
   
   const parts = hostname.split('.');
-  
   const subdomain = parts.length >= 3 ? parts[0] : null;
-
-  
   const validSubdomains = ['bot', 'free', 'freex1', 'vip', 'vipx1'];
 
-  
-  if (subdomain && validSubdomains.includes(subdomain)) {
-    const pathname = url.pathname;
+ 
+  if (
+    pathname.startsWith('/_astro') || 
+    pathname.includes('favicon') ||
+    (subdomain && pathname.startsWith(`/${subdomain}`)) ||
+    (subdomain && pathname.startsWith(`/es/${subdomain}`))
+  ) {
+    return next();
+  }
 
-  
+ 
+  if (subdomain && validSubdomains.includes(subdomain)) {
+    
     if (pathname.startsWith('/es')) {
-      
       const pathWithoutEs = pathname.replace('/es', '');
-     
       const cleanPath = pathWithoutEs === '' || pathWithoutEs === '/' ? '/' : pathWithoutEs;
       
      
       return context.rewrite(`/es/${subdomain}${cleanPath}`);
     }
 
+   
     return context.rewrite(`/${subdomain}${pathname}`);
   }
 
-  // Si no es un subdominio (ej: eldoradofx.co.uk), que siga normal
   return next();
 });
